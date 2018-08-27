14a8413871940d8a73f6a8987df166587184289d
kevahere <kevahere@gmail.com>
2018-08-27 08:21:36 +0300

    added test for credentials init

--- credentials_test.py
+++ credentials_test.py
@@ -2,3 +2,18 @@ import unittest
 import pyperclip
 from credentials import Credentials
 
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+        '''runs before each test case'''
+        self.new_credentials = Credentials("kevahere","3nd0fDay5")
+
+    def test_init(self):
+        self.assertEqual(self.new_credentials.username,"kevahere")
+        self.assertEqual(self.new_credentials.password,"3nd0fDay5")
+
+    def tearDown(self):
+        Credentials.credentials_list = []
+
+if __name__ == '__main__':
+    unittest.main()
